729f2cd2bd2de1aacab8aaa4308e1109d3906abd
Stanotech <satach@o2.pl>
2025-03-31 00:45:57 +0200

    fixed

--- app/scripts/palindrom.py
+++ app/scripts/palindrom.py
@@ -1,18 +1,24 @@
-s = "fdsaghdshfvdabcdefedcba"
+s = "fdsbaabghdshfvdabcdefedcba"
+longest = ""
 
-for i in range(len(s) - 1):
-        for even in (True, False):  # Sprawdzamy zarówno parzyste, jak i nieparzyste palindromy
-            length = 2 if even else 3
-            j = 1
-            try:
-                while True:
-                    left = i - j if even else i - j - 1
-                    right = i + j + 1
-                    if s[left] == s[right]:
-                        length += 2
-                        j += 1
-                    else:
-                        break
-            except IndexError:
-                print(s[int(i - length / 2 + 1):int(i + length / 2 + 1)])
-                continue
+for i in range(len(s)):
+    for even in (True, False):  # Sprawdzamy zarówno parzyste, jak i nieparzyste palindromy
+        length = 2 if even else 1
+        j = 0
+        
+        while True:
+            left = i - j if even else i - j
+            right = i + j + 1
+            
+            if left < 0 or right >= len(s) or s[left] != s[right]:
+                break
+
+            length += 2
+            j += 1
+
+        # Aktualizujemy najdłuższy palindrom
+        palindrome = s[left + 1:right]  # Poprawny zakres podciągu
+        if len(palindrome) > len(longest):
+            longest = palindrome
+
+print("Najdłuższy palindrom:", longest)
