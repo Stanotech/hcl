4bd7e22dfe61c573b88b46e3e40105d1c9a0a20d
Stanotech <satach@o2.pl>
2025-03-31 01:56:22 +0200

    fix

--- app/scripts/target_sum.py
+++ app/scripts/target_sum.py
@@ -1,5 +1,5 @@
-nums = [-10, -5, 0, 3, 4, 5, 7, 9, 10, 15]
-target = 10
+nums = [-20, -15, -10, -5, 0, 2, 3, 6, 7, 8, 9, 12, 14, 16, 18, 21, 23]
+target = 18
 
 idx1= 0
 idx2= len(nums)-1
@@ -9,7 +9,6 @@ while idx1 < idx2:
     if nums[idx1] + nums[idx2] == target:
         print(f"pair of numbers: {nums[idx1]} & {nums[idx2]}")
         idx2 -= 1
-        idx1 -= 1
 
     elif nums[idx1] + nums[idx2] < target:
         idx1 += 1
