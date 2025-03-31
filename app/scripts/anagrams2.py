50aa61e8bcf5e48b8311adc32c39d2106b00f758
Stanotech <satach@o2.pl>
2025-03-31 15:18:05 +0200

    anagrams2

---
+++ app/scripts/anagrams2.py
@@ -0,0 +1,11 @@
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+anagrams = {}
+
+for word in words:
+    sorted_word = ''.join(sorted(word))
+    
+    if sorted_word not in anagrams:
+        anagrams[sorted_word] = []
+    anagrams[sorted_word].append(word)    
+
+print(list(anagrams.values()))
