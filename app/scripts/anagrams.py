56fd298bd31b66416e68451758c7b52d2551831e
Stanotech <satach@o2.pl>
2025-03-31 11:41:29 +0200

    anagrams

---
+++ app/scripts/anagrams.py
@@ -0,0 +1,15 @@
+s = "cbaebabacdacbca"
+target = "abc"
+
+for i in range(len(s)):
+    anagram = s[i:i+len(target)]
+    for char in target:  
+        idx = anagram.find(char)
+        if idx != -1:
+            anagram = anagram[:idx]+ anagram[idx+1:]
+            if len(anagram) == 0 and len(s[i:i+len(target)]) == 3:
+                print(s[i:i+len(target)])
+        else:
+            break
+
+            
