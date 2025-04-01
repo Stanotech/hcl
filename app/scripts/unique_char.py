deb422d26e4c85f34471b5c662bd825660dba921
Stanotech <satach@o2.pl>
2025-04-01 09:14:20 +0200

    unique_char

---
+++ app/scripts/unique_char.py
@@ -0,0 +1,20 @@
+def first_unique_char(str):
+    dic = {}
+    for idx, char in enumerate(str):
+        if char in dic:
+            dic[char].append(idx)
+        else:
+            dic[char] = [idx]
+
+    for key, value in dic.items():
+        if len(value) == 1: 
+            return value[0]
+    
+    else:
+        return -1
+
+
+
+print(first_unique_char("loveleetcode"))  # 2  (bo 'v' jest pierwszym unikalnym znakiem)
+print(first_unique_char("leetcode"))  # 0  (bo 'l' jest pierwszym niepowtarzającym się znakiem)
+print(first_unique_char("aabb"))  # -1  (bo nie ma unikalnych znaków)
