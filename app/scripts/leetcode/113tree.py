fe00d3111ead37e950e5c01bca1268e9055034a0
Stanotech <satach@o2.pl>
2025-06-03 13:13:25 +0200

    tree update

--- app/scripts/leetcode/113tree.py
+++ app/scripts/leetcode/113tree.py
@@ -17,7 +17,7 @@ class Solution(object):
             current_sum += node.val
             
             if not node.left and not node.right and current_sum == targetSum:
-                result.append(list(current_path))  # robimy kopię listy
+                result.append(current_path.copy())
             
             dfs(node.left, current_path, current_sum)
             dfs(node.right, current_path, current_sum)
